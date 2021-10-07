cfa7162403db08c1a5ff8781d0415913c09e47e6
shane <willyums86@gmail.com>
2021-10-07 17:01:11 -0400

    Adds Auth

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Signup from "../views/Signup.vue";
+import Login from "../views/Login.vue";
+import Logout from "../views/Logout.vue";
+import ProductsIndex from "../views/ProductsIndex.vue";
 
 Vue.use(VueRouter);
 
@@ -11,14 +15,13 @@ const routes = [
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: "/signup",
+    name: "signup",
+    component: Signup,
   },
+  { path: "/login", name: "login", component: Login },
+  { path: "/logout", name: "logout", component: Logout },
+  { path: "/products", name: "products-index", component: ProductsIndex },
 ];
 
 const router = new VueRouter({
---
+++ src/views/ProductsIndex.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="products-index">
+    <h1>All Products</h1>
+    <div v-for="product in products" v-bind:key="product.id">
+      <h4>{{ product.name }}</h4>
+      <p>description: {{ product.description }}</p>
+      <router-link :to="`/product/${product.id}`">See Details</router-link>
+    </div>
+  </div>
+</template>
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      products: [],
+    };
+  },
+  created: function () {
+    axios.get("/products").then((response) => {
+      console.log(response.data);
+      this.products = response.data;
+    });
+  },
+  methods: {},
+};
+</script>
